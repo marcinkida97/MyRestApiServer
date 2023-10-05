9a7da031f0e40540fdd52ca2250f0840271003d0
marcinkida97 <mkida97@gmail.com>
2023-10-05 19:47:19 +0200

    First commit

---
+++ src/main/java/com/example/myrestapiserver/myrestapiserver/MyRestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myrestapiserver.myrestapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRestApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myrestapiserver/myrestapiserver/MyRestApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrestapiserver.myrestapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
